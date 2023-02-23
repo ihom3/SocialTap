239bc3bed480b852ea8f03c62edc3c6c87d43be2
kjgnapp <73502423+kjgnapp@users.noreply.github.com>
2023-02-22 23:43:49 -0500

    Update user.go for Auth0

--- server/user.go
+++ server/user.go
@@ -4,38 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
-	"os"
-	
 	"github.com/gorilla/mux"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// database variable
 var DB *gorm.DB
-
-// error variable
 var err error
 
-// database url (whoever is testing it, don't forget to change this to your local connection)
-//const DNS = "root:<password>@tcp(127.0.0.1:3306)/social_tap?charset=utf8mb4&parseTime=True&loc=Local"
+const DNS = "root:Kujo2022!@tcp(127.0.0.1:3306)/social_tap?charset=utf8mb4&parseTime=True&loc=Local"
 
-//to protect the password we generate an environmental variable
-var pswd = os.Getenv("MYSQL_PASSWORD")
-var DNS = "root:" + pswd + "@tcp(127.0.0.1:3306)/social_tap?charset=utf8mb4&parseTime=True&loc=Local"
-
-// creating a struct(class) to store the different data types in order for us to be able to save these data in the databse
 type User struct {
-	gorm.Model `json:"-"`
-	UserName   string `json:"username"`
-	FirstName  string `json: "firstname"`
-	LastName   string `json: "lastname"`
+	gorm.Model
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
 }
 
-// once the model is created we are going to initialize the database and enable automigration
 func InitialMigration() {
-	//here we will define our database details
 	DB, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
@@ -44,9 +30,6 @@ func InitialMigration() {
 	DB.AutoMigrate(&User{})
 }
 
-//defining the handler functions
-
-// getting all users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var users []User
@@ -54,29 +37,22 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-// getting one user
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Vars returns the route variables for the current request
+	params := mux.Vars(r)
 	var user User
 	DB.First(&user, params["id"])
 	json.NewEncoder(w).Encode(user)
 }
 
-// creating a user
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
-	//decoding the data from the body of the request
-	//whatever data we're getting we are decoding it with a reference to user
 	json.NewDecoder(r.Body).Decode(&user)
-	//to save the data in the database
 	DB.Create(&user)
-	//to parse the data back to the browser, w=response writer
 	json.NewEncoder(w).Encode(user)
 }
 
-// updating information about a user
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -87,11 +63,10 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-// deleting a user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	var user User
 	DB.Delete(&user, params["id"])
-	json.NewEncoder(w).Encode("The user is deleted successfully!")
+	json.NewEncoder(w).Encode("The user is Deleted Successfully")
 }
