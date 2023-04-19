0d1bb3f04f145b63450b15f4ee100e3badfbeb05
kjgnapp <73502423+kjgnapp@users.noreply.github.com>
2023-04-19 02:40:21 -0400

    Update connection.go
    Database.go Update.

--- server/database/connection.go
+++ server/database/connection.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"backend/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:rootroot@/social_tap"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database!")
+	}
+	connection.AutoMigrate(&models.User{}, &models.UnregisteredUser{})
+
+	DB = connection
+}
