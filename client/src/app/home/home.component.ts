a050c96fe707e23086bc72ab758121e8a0f7480c
ihom3 <62973324+ihom3@users.noreply.github.com>
2023-04-19 11:31:45 -0400

    Updated Home Page, added group member pictures

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
+  host: {'class': 'container'}
 })
 export class HomeComponent {
   gitHub = (): void  => {
