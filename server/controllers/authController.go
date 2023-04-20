99f2d3a795703c81ab7ad7790329918004c1d74c
kjgnapp <73502423+kjgnapp@users.noreply.github.com>
2023-04-19 21:44:27 -0400

    Update authController.go
    Controller Functions

--- server/controllers/authController.go
+++ server/controllers/authController.go
@@ -17,18 +17,286 @@ import (
 )
 
 const JWTSecret = "secret"
-.
-.
-.
-.
-.
-.
-.
-.
-.
 
+func Register(c *fiber.Ctx) error {
+	//parse user input
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
 
-func UpdatePassword(c *fiber.Ctx) error {
+	//check if user is unregistered
+	unregistered := database.DB.Find(&models.UnregisteredUser{}, "code = ?", data["code"])
+	if unregistered.RowsAffected == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid code",
+		})
+	}
+	//hash password
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	//create user object
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  password,
+		Code:      data["code"],
+	}
+	//create user in database
+	err := database.DB.Create(&user)
+	if err.Error != nil {
+		return c.Status(400).JSON(err.Error)
+	}
+	//remove unregistered code
+	database.DB.Delete(&models.UnregisteredUser{}, "code = ?", data["code"])
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	//check if user is already logged in
+
+	cookieCheck := c.Cookies("jwt")
+	_, CheckLoginError := jwt.ParseWithClaims(cookieCheck, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTSecret), nil
+	})
+	if CheckLoginError == nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "User is Already Logged In",
+		})
+	}
+	//parse user input
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	var user models.User
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.ID == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(JWTSecret))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "Could not log in",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func RegisterNewCode(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTSecret), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	if user.Role != "admin" {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	unregisteredCheck := database.DB.Find(&models.UnregisteredUser{}, "code = ?", data["code"])
+	if unregisteredCheck.RowsAffected != 0 {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Code Already Exists",
+		})
+	}
+	registeredCHeck := database.DB.Find(&models.User{}, "code = ?", data["code"])
+	if registeredCHeck.RowsAffected != 0 {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Code Already Exists",
+		})
+	}
+	unregistered := models.UnregisteredUser{
+		Code: data["code"],
+	}
+	database.DB.Create(&unregistered)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func IDRoute(c *fiber.Ctx) error {
+	code := c.Params("id")
+	if code == "" {
+		return c.JSON(fiber.Map{
+			"message": "User Not Found",
+		})
+	}
+	var user models.User
+	err := database.DB.Preload("Socials").First(&user, "code = ?", code)
+	if err.RowsAffected == 0 {
+		unregistered := database.DB.First(&models.UnregisteredUser{}, "code = ?", code)
+		if unregistered.RowsAffected == 0 {
+			return c.JSON(fiber.Map{
+				"message": "User Not Found",
+			})
+		}
+		return c.JSON(fiber.Map{
+			"message": "User Not Registered",
+		})
+	}
+	return c.JSON(user)
+}
+
+func UpdateBio(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTSecret), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	if user.ID == 0 {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	database.DB.Model(&user).Update("bio_text", data["bio_text"])
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func UpdateName(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTSecret), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	if user.ID == 0 {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	database.DB.Model(&user).Update("first_name", data["first_name"])
+	database.DB.Model(&user).Update("last_name", data["last_name"])
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func UpdateEmail(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTSecret), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	if user.ID == 0 {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	if err := database.DB.Model(&user).Update("email", data["email"]); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Email Already Exists",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}func UpdatePassword(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(JWTSecret), nil
